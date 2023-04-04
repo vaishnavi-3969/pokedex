8eebe162c66126d7816ff07d196584f99ffb583b
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-04-04 21:28:37 +0530

    Initialize project using Create React App

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header
+      style={{ backgroundColor: "#3CD3AD", color: "#000", padding: "1rem" }}
+    >
+      <h1 style={{ textAlign: "center", margin: 0 }}>Pokemon Dashboard</h1>
+    </header>
+  );
+};
+
+export default Header;
